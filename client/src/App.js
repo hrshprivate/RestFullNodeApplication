094f25ea82a0189c4985650b385889ccdf70413d
hrsh1337 <93730817+hrshprivate@users.noreply.github.com>
2022-05-21 22:41:54 +0300

    bad_react

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,10 @@
 import React, { useEffect, useState } from 'react'
-
+import Card from 'react-bootstrap/Card'
+import Button from 'react-bootstrap/Button'
 import './App.css'
 
 function App() {
-  const [data, setData] = useState(null)
+  const [data, setData] = useState([])
 
   useEffect(() => {
     fetch('http://localhost:5000/api/posts/')
@@ -12,9 +13,17 @@ function App() {
   }, [])
 
   return (
-    <div className="file-manager">
-      <p>{!data ? 'Loading...' : `${data[0].author}`}</p>
-    </div>
+    <Card style={{ width: '18rem' }}>
+      <Card.Img
+        variant="top"
+        src={process.env.REACT_APP_API_URL + data[0].picture}
+      />
+      <Card.Body>
+        <Card.Title>Read</Card.Title>
+        <Card.Text>axscdvfgbfds</Card.Text>
+        <Button variant="primary">:D</Button>
+      </Card.Body>
+    </Card>
   )
 }
 
